84af2b2a30727fe2487d0bfa67d934e79f9c1b70
unknown <chandral.thakor@gmail.com>
2020-01-05 15:11:20 +0530

    Population layer

--- app.py
+++ app.py
@@ -20,9 +20,15 @@ my_map = folium.Map(location=[38.58, -99.09], zoom_start=5, tiles="openstreetmap
 fgv = folium.FeatureGroup(name="Volcanoes")
 for lt, ln, el in zip(lat, lon, elev):
     fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el) + ' m', fill_color=color_producer(el),
-                                      fill=True, color='grey', fill_opacity=0.7))
+                                      fill=False, color='grey', fill_opacity=0.7))
+
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('data/world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x:
+{'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005']
+< 20000000 else 'red'}))
 
 
 my_map.add_child(fgv)
+my_map.add_child(fgp)
 my_map.add_child(folium.LayerControl())
 my_map.save("Map1.html")
